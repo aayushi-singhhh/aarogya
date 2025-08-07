9f40928a76fd4e08f51268f4132435c765e88463
Aayushi Singh <aayushisingh239@gmail.com>
2025-08-07 23:05:14 +0530

    did some changes like adding flipping words and hero text

--- aarogya/src/components/parallaxBackground.jsx
+++ aarogya/src/components/parallaxBackground.jsx
@@ -1,4 +1,5 @@
-import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
 
 const ParallaxBackground = () => {
   const { scrollYProgress } = useScroll();
