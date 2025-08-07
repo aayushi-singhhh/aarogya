ee9bb4c6ce16d873657f5dae451535a900f77f1f
Aayushi Singh <aayushisingh239@gmail.com>
2025-08-07 21:47:34 +0530

    made a new loader and also update the background and parallaxBackground

--- aarogya/src/App.jsx
+++ aarogya/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./sections/navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
 
-export default App
+export default App;
